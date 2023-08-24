f1e1c442486d9c610ec4782814c4ded83e741b3b
Ala Farjallah <farjallahala37@gmail.com>
2023-08-24 23:45:54 +0100

    Final-Project

--- js/main.js
+++ js/main.js
@@ -1,8 +1,12 @@
+// Select the header element
 const header = document.querySelector("header");
 
-window.addEventListener ("scroll",function(){
-    header.classList.toggle ("sticky",this.window.scrollY> 0);
-})
+// Add a scroll event listener to the window
+window.addEventListener("scroll", function () {
+    // Toggle the "sticky" class on the header based on the scroll position
+    header.classList.toggle("sticky", this.window.scrollY > 0);
+});
 
+// Select the menu icon and navigation menu
 let menu = document.querySelector('#nav-icon');
 let navmenu = document.querySelector('navbar');
